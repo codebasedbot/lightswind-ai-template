34afd7b8f0dfd6ce24ad2ae7ac5a1814e4a3163b
codebasedbot <codebasedev788@gmail.com>
2025-12-04 14:11:12 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Index = () => {
     return (
-        <div>
-
+        <div className="flex h-screen items-center justify-center">
+             <h1 className="text-5xl text-red-500">Hello Coder....!</h1>
         </div>
     )
 }
